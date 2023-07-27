045b7e2aae53583ab0476adce6ad8f619b7d9c6a
assaf21 <assaf.vardy@gmail.com>
2023-07-27 09:22:37 +0300

    backend - login endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,9 @@
 import express from 'express';
-import {connection} from '../connection.js'
+import {connection} from '../connection.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const router = express.Router();
 
 router.post('/signup', (req, res)=>{
@@ -24,5 +28,28 @@ router.post('/signup', (req, res)=>{
         }   
     })
 })
+router.post('/login', (req, res) => {
+   const {email, password} = req.body?.user;
+   const query = 'select email, password, role, status from user where email= ?';
+   connection.query(query,[email], (err,response)=>{
+    if(!err){
+        if ((Array.isArray(response) && response.length === 0) || response[0].password !== password){
+            res.status(401).json({message:'Incorrect user or password'});
+        }else if( response[0].status === 'false' ){
+            res.status(401).json({message:'You are not authorized yet. Please wait for admin approval'});
+        }else if ( response[0].password === password){
+            const result = {email:response[0].email, role:response[0].role}
+            const accessToken = jwt.sign(result, process.env.ACCESS_TOKEN, {expiresIn:'8h'});
+            res.status(200).json({token:accessToken});
+        }else{
+            res.status(400).json({message:'Something went wrong. Please try again'});
+        }
+    }else{
+        res.status(500).json(err);
+    }
+    
+   });
+});
+
 
 export default router;
